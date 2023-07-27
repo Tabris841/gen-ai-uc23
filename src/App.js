e9e5911d34f2a508c5521c9fecc5f2bb32727b93
Anton Todorov <shadowtabris84@gmail.com>
2023-07-27 11:09:25 +0300

    UC#23 Generate test data

--- src/App.js
+++ src/App.js
@@ -2,11 +2,73 @@ import { faker } from '@faker-js/faker';
 
 import './App.css';
 
-const AGE_CERTIFICATION = ["G", "PG", "PG-13", "R", "NC-17", "U", "U/A", "A", "S", "AL", "6", "9", "12", "12A", "15", "18", "18R", "R18", "R21", "M", "MA15+", "R16", "R18+", "X18", "T", "E", "E10+", "EC", "C", "CA", "GP", "M/PG", "TV-Y", "TV-Y7", "TV-G", "TV-PG", "TV-14", "TV-MA"];
-const GENRES = ["Action", "Adventure", "Comedy", "Drama", "Fantasy", "Horror", "Mystery", "Romance", "Thriller", "Western", "Sci-Fi", "Biography", "History"];
-const ROLES = ["Director", "Producer", "Screenwriter", "Actor", "Actress", "Cinematographer", "Film Editor", "Production Designer", "Costume Designer", "Music Composer"];
-const TITLE_HEADERS = ['id', 'title', 'description', 'release_year', 'age_certification', 'runtime', 'genres', 'production_country', 'seasons'];
-const CREDIT_HEADERS = ['id', 'title_id', 'real_name', 'character_name', 'role']
+const AGE_CERTIFICATION = [
+  'G',
+  'PG',
+  'PG-13',
+  'R',
+  'NC-17',
+  'U',
+  'U/A',
+  'A',
+  'S',
+  'AL',
+  '6',
+  '9',
+  '12',
+  '12A',
+  '15',
+  '18',
+  '18R',
+  'R18',
+  'R21',
+  'M',
+  'MA15+',
+  'R16',
+  'R18+',
+  'X18',
+  'T',
+  'E',
+  'E10+',
+  'EC',
+  'C',
+  'CA',
+  'GP',
+  'M/PG',
+  'TV-Y',
+  'TV-Y7',
+  'TV-G',
+  'TV-PG',
+  'TV-14',
+  'TV-MA',
+];
+const GENRES = [
+  'Action',
+  'Adventure',
+  'Comedy',
+  'Drama',
+  'Fantasy',
+  'Horror',
+  'Mystery',
+  'Romance',
+  'Thriller',
+  'Western',
+  'Sci-Fi',
+  'Biography',
+  'History',
+];
+const ROLES = [
+  'Director',
+  'Producer',
+  'Screenwriter',
+  'Actor',
+  'Actress',
+  'Cinematographer',
+  'Film Editor',
+  'Production Designer',
+  'Costume Designer',
+  'Music Composer',
+];
 
 function App() {
   function getRandomElement(collection) {
@@ -23,25 +85,31 @@ function App() {
       id: i + 1,
       title: faker.lorem.words(3),
       description: faker.lorem.sentences(3),
-      release_year: faker.date.between({ from: '1900-01-01', to:new Date()}).getFullYear(),
+      release_year: faker.date
+        .between({ from: '1900-01-01', to: new Date() })
+        .getFullYear(),
       age_certification: getRandomElement(AGE_CERTIFICATION),
       runtime: getRandomNumberInRange(80, 200),
-      genres: [...Array(getRandomNumberInRange(1, 3))].map(() => getRandomElement(GENRES)),
+      genres: [...Array(getRandomNumberInRange(1, 3))].map(() =>
+        getRandomElement(GENRES)
+      ),
       production_country: faker.location.countryCode(),
       seasons: Math.random() > 0.7 ? getRandomNumberInRange(1, 10) : null,
     }));
 
-    const credits = titles.flatMap((title) => [...Array(getRandomNumberInRange(5, 10))].map((_, i) => ({
-      id: i + 1 + title.id * 10,
-      title_id: title.id,
-      real_name: faker.person.fullName(),
-      character_name: faker.person.fullName(),
-      role: getRandomElement(ROLES),
-    })));
+    const credits = titles.flatMap((title) =>
+      [...Array(getRandomNumberInRange(5, 10))].map((_, i) => ({
+        id: i + 1 + title.id * 10,
+        title_id: title.id,
+        real_name: faker.person.fullName(),
+        character_name: faker.person.fullName(),
+        role: getRandomElement(ROLES),
+      }))
+    );
 
-    console.log({titles, credits})
+    console.log({ titles, credits });
 
-    return {titles, credits}
+    return { titles, credits };
   }
 
   function arrayToCSV(data) {
@@ -50,8 +118,8 @@ function App() {
     csvRows.push(headers.join(','));
 
     for (const row of data) {
-      const values = headers.map(header => {
-        const escaped = (''+row[header]).replace(/"/g, '\\"');
+      const values = headers.map((header) => {
+        const escaped = ('' + row[header]).replace(/"/g, '\\"');
         return `"${escaped}"`;
       });
       csvRows.push(values.join(','));
@@ -61,11 +129,13 @@ function App() {
   }
 
   function generateAndDownloadData() {
-    const {credits, titles} = generateData()
+    const { credits, titles } = generateData();
 
     const csvTitlesData = arrayToCSV(titles);
     const csvCreditsData = arrayToCSV(credits);
-    const blob = new Blob([csvTitlesData, csvCreditsData], { type: 'text/csv' });
+    const blob = new Blob([csvTitlesData, csvCreditsData], {
+      type: 'text/csv',
+    });
     const url = URL.createObjectURL(blob);
 
     const link = document.createElement('a');
@@ -78,7 +148,9 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={generateAndDownloadData}>Perform synthetic data generation</button>
+      <button onClick={generateAndDownloadData}>
+        Perform synthetic data generation
+      </button>
     </div>
   );
 }
